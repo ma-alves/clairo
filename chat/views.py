d7dcdfdd8fba7a74720837b13dd8f0f01cccf987
matheus <matheusamenos@gmail.com>
2025-09-16 15:41:31 -0300

    Ajustes na base e no login

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect, render
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 def home(request):
@@ -13,9 +17,12 @@ class UserDetailView(DetailView):
     template_name = "chat/profile.html"
 
 
-def signup(request):
-    ...
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 
-def logout(request):
-    ...
+def logout_view(request):
+    logout(request)
+    return redirect("home")
