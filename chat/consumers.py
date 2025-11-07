efcefade0bd04ce8e63921282600627f862dba23
matheus <matheusamenos@gmail.com>
2025-11-07 01:01:26 -0300

    Implementação inicial de OnlineConsumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -44,6 +44,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from WebSocket/Client
+    # essa porra recebe a mensagem do cliente, processa o caralho que for e aí sim entrega pro grupo
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         body_message = text_data_json["message"]
@@ -51,7 +52,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.create_message(self.room_name, self.user, body_message)
 
-        # Send message to room group/Broadcast para todos os consumidores no grupo
+        # Send the message that came from the client to room group/Broadcast para todos os consumidores no grupo
         await self.channel_layer.group_send(
             self.room_group_name, {
                 "type": "chat.message",
@@ -60,7 +61,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group/Broadcast do grupo
+    # Client receives message from room group/Broadcast do grupo
     async def chat_message(self, event):
         message = event["message"]
         user = event["user"]
@@ -72,3 +73,69 @@ class ChatConsumer(AsyncWebsocketConsumer):
             "user": user
         })
         )
+
+
+class OnlineConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"] #type: ignore
+        self.room_group_name = "online_users"
+
+        await self.channel_layer.group_add(
+            self.room_group_name, self.channel_name
+        )
+        await self.set_user_online(True)
+        await self.accept()
+
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
+        await self.set_user_online(False)
+
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message_type = text_data_json.get("type")
+
+        # talvez fazer a coisa de checar online aqui
+        if message_type == "check_user_status":
+            user_id = text_data_json.get("user_id")
+            if user_id:
+                is_online = await self.check_user_online_status(user_id)
+                await self.send(text_data=json.dumps({
+                    "type": "user_status_response",
+                    "user_id": user_id,
+                    "is_online": is_online
+                }))
+
+
+    async def user_status(self, event):
+        user = event["user"]
+        status = event["status"]
+
+        await self.send(text_data=json.dumps({
+            "user": user,
+            "status": status
+        }))
+    
+    
+    @database_sync_to_async
+    def set_user_online(self, is_online):
+        if is_online:
+            user_chats = Chat.objects.filter(users=self.user)
+            for chat in user_chats:
+                chat.online_users.add(self.user)
+        else:
+            user_chats = Chat.objects.filter(users=self.user)
+            for chat in user_chats:
+                chat.online_users.remove(self.user)
+
+
+    @database_sync_to_async
+    def check_user_online_status(self, user_id):
+        user_chats = Chat.objects.filter(users__id=user_id)
+        for chat in user_chats:
+            if chat.online_users.filter(id=user_id).exists():
+                return True
+        return False
