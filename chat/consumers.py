395e91986437a160bf25053fe1e1cb4561f59ed8
matheus <matheusamenos@gmail.com>
2025-11-14 12:12:08 -0300

    Novos testes de OnlineConsumer e exceptions

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,6 +4,7 @@ import json
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+from chat.exceptions import ClientError
 from chat.models import Chat, ChatMessage, UserOnlineStatus
 
 
@@ -27,8 +28,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # recebe a mensagem do cliente, processa o que for e aí sim entrega pro grupo
     # chatSocket.send()
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        body_message = text_data_json["message"]
+        data = json.loads(text_data)
+        body_message = data["message"]
 
         await self.create_message(self.room_name, self.user, body_message)
         # Send the message that came from the client to room group/Broadcast para todos os consumidores no grupo
@@ -81,10 +82,14 @@ class OnlineConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        connection_type = data.get("type")
-        message_user_id = data.get("user_id")
+        try:
+            connection_type, message_user_id = self.check_text_data(data)
+        except ClientError as e:
+            await self.send(text_data=json.dumps({
+                "error": e.code
+            }))
+            return
 
-        await self.set_user_status(message_user_id, connection_type)
         await self.channel_layer.group_send(
             self.room_group_name, {
                 "type": "user.status",
@@ -92,20 +97,29 @@ class OnlineConsumer(AsyncWebsocketConsumer):
                 "user_id": message_user_id
             }
         )
+        await self.set_user_status(message_user_id, connection_type)
 
-    # AQUI
-    # Note that the event you send must have a type key, even if only one
-    # type of message is being sent over the channel, as it will turn into
-    # an event a consumer has to handle.
     async def user_status(self, event):
         status = True if event["status"] == "open" else False
         user_id = event["user_id"]
-
+            
         await self.send(text_data=json.dumps({
             "status": status,
             "user_id": user_id,
         }))
-    
+
+    def check_text_data(self, data):
+        connection_type = data.get("type")
+        message_user_id = data.get("user_id")
+
+        if connection_type not in ["open", "closed"]:
+            raise ClientError("STATUS INVÁLIDO")
+        
+        if not isinstance(message_user_id, int):
+            raise ClientError("ID INVÁLIDO")
+        
+        return connection_type, message_user_id
+
     @database_sync_to_async
     def set_user_status(self, user_id, status):
         try:
--- chat/tests.py
+++ chat/tests.py
@@ -4,15 +4,17 @@ from chat.models import Chat
 from channels.auth import AuthMiddlewareStack
 from channels.routing import URLRouter
 from channels.testing import WebsocketCommunicator
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase
 from django.urls import path
 
 
 class OnlineTestCase(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpTestData(cls):
         User.objects.create_user(username="usuario_de_teste", password="Senha1234!")
-        self.application = AuthMiddlewareStack(OnlineConsumer.as_asgi())
+        cls.anom_user = AnonymousUser()
+        cls.application = AuthMiddlewareStack(OnlineConsumer.as_asgi())
 
     async def test_online_connect(self):
         communicator = WebsocketCommunicator(self.application, "/ws/online-status/")
@@ -31,6 +33,26 @@ class OnlineTestCase(TestCase):
         await communicator.send_json_to({"type": "closed", "user_id": 1})
         response = await communicator.receive_json_from()
         assert response == {"status": False, "user_id": 1}
+        
+        await communicator.disconnect()
+
+    async def test_type_error(self):
+        communicator = WebsocketCommunicator(self.application, "/ws/online-status/")
+        await communicator.connect()
+        await communicator.send_json_to({"type": "not_a_type", "user_id": 1})
+        response = await communicator.receive_json_from()
+        
+        assert response == {"error": "STATUS INVÁLIDO"}
+        
+        await communicator.disconnect()
+
+    async def test_user_error(self):
+        communicator = WebsocketCommunicator(self.application, "/ws/online-status/")
+        await communicator.connect()
+        await communicator.send_json_to({"type": "open", "user_id": "not_an_int"})
+        response = await communicator.receive_json_from()
+
+        assert response == {"error": "ID INVÁLIDO"}
         
         await communicator.disconnect()
 
@@ -72,3 +94,4 @@ class ChatTestCase(TestCase):
         assert "time" in response
 
         await communicator.disconnect()
+        
