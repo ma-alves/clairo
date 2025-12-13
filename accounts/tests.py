550bd7bc519aebcd125402da0ca974ae63db11e5
matheus <matheusamenos@gmail.com>
2025-12-12 23:03:59 -0300

    Adiciona novos testes em accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
 from accounts.utils import cipher_suite
 
@@ -19,3 +20,33 @@ class UserAuthTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(decrypted_token, response.context['token'])
+
+    def test_update_password_view(self):
+        self.client.force_login(self.user1)
+        response = self.client.post(
+            '/accounts/update-password/'
+            , {
+                'old_password': 'Senha1234!',
+                'new_password1': 'NovaSenha1234!',
+                'new_password2': 'NovaSenha1234!',
+            }
+            )
+
+        self.assertRedirects(response, reverse('home'))
+        self.user1.refresh_from_db()
+        self.assertTrue(self.user1.check_password('NovaSenha1234!'))
+
+    def test_reset_password_view(self):
+        self.client.force_login(self.user1)
+        response = self.client.post(
+            '/accounts/reset-password/'
+            , {
+                'new_password1': 'RedefinirSenha1234!',
+                'new_password2': 'RedefinirSenha1234!', 
+            }
+            )
+        
+        self.assertRedirects(response, reverse('home'))
+        self.user1.refresh_from_db()
+        self.assertTrue(self.user1.check_password('RedefinirSenha1234!'))
+    
